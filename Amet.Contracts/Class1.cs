31ed1fd0c38eda000ea02666370c34720c12101f
Juan Rene Guzman Nunez <juanreneguzman23@gmail.com>
2021-09-23 11:59:50 -0400

    proyect

---
+++ Amet.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Amet.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ Amet.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Amet.Models
+{
+    public class Class1
+    {
+    }
+}
