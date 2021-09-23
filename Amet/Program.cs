86bd5e60ee1cc994be943f2b74036a4dfd64a0a9
Juan Rene Guzman Nunez <juanreneguzman23@gmail.com>
2021-09-22 20:51:00 -0400

    FIRST COMMIT

---
+++ Amet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Amet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BIENVENIDO A AMET ");
+        }
+    }
+}
